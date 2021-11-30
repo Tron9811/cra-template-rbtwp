bf9ad99e546ea34ac4dc73dcc6dec2ea36caae16
naidsha <naidsha@emea.corpdir.net>
2021-11-30 09:29:04 +0200

    intial commit

---
+++ template/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Bundle from './Components/Bundle/Bundle';
+
+import './styles/index.css'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Bundle/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
